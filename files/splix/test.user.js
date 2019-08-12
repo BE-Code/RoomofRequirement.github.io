2cbfd8aa5e3c8cf292921830ae05fb830ea1ac84
BE-Code <43590516+BE-Code@users.noreply.github.com>
2019-08-12 14:40:25 -0600

    free crawl, title adjustments, +minor changes

--- files/splix/test.user.js
+++ files/splix/test.user.js
@@ -10,16 +10,21 @@
 
 var boxPattern = false;
 var crawlPattern = false;
+var freeCrawl = false;
 
 function resetPatterns() {
     boxPattern = false;
     crawlPattern = false;
+    freeCrawl = false;
 }
+
 
 (function() {
     $.noConflict();
     window.addEventListener("load", function(){
 
+
+        // Define Patterns
         setInterval(function(){
             if(boxPattern) {
                 sendDir((myPlayer.dir + 1) % 4);
@@ -35,44 +40,50 @@ function resetPatterns() {
             }
         }, 450);
 
+        var freeCrawlDir = 0;
+        setInterval(function(){
+            if(freeCrawl) {
+                sendDir(4);
+                setTimeout(function(){
+                    sendDir((freeCrawlDir + 1) % 4);
+                }, 100);
+            }
+        }, 30000);
+
+
+        // Check Keys
         var checkCrawlKey = false;
         window.addEventListener("keyup", function(e){
-            resetPatterns();
-            if(!checkCrawlKey) {
-                if(e.keyCode == 66) boxPattern = true;
-                else if(e.keyCode == 67) checkCrawlKey = true;
+            if (e.keyCode != 79) resetPatterns();
+            if (!checkCrawlKey) {
+                if (e.keyCode == 80) sendDir(4);
+                else if (e.keyCode == 66) boxPattern = true;
+                else if (e.keyCode == 67) checkCrawlKey = true;
             }
             else {
-                if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) {
-                    crawlPattern = true; // left 2. up 3. right 0. down 1.
+                if (e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) crawlPattern = true; // left 2. up 3. right 0. down 1.
+                else if (e.keyCode == 67) {
+                    crawlPattern = true;
+                    freeCrawl = true;
                 }
                 checkCrawlKey = false;
             }
         });
 
-        var curRank = 333; //Arbitrary (anything higher than 100 works fine)
-        var newRank = curRank;
+
+        // Title Bar
+        var curRank = 0;
+        var totalRanks = 0;
         var numKills = 0;
         setInterval(function(){
-            newRank = Number(document.getElementById("myRank").innerHTML);
+            curRank = Number(document.getElementById("myRank").innerHTML);
+            totalRanks = Number(document.getElementById("totalPlayers").innerHTML);
             numKills = Number(document.getElementById("myKills").innerHTML);
-            if ((newRank < curRank) && (newRank != 0) && (newRank < 10)) {
-                curRank = newRank;
-                //alert("rank up to " + newRank);
-            }
         }, 1000);
 
-        var timer = "";
-        var flasher;
-        window.onblur = function() {
-            flasher = setInterval(function() {
-                document.title = (document.title.startsWith("Kills: ")) ? ("Rank: " + newRank) : ("Kills: " + numKills);
-            }, 30000);
-        }
-        window.onfocus=function() {
-            clearInterval(flasher);
-            document.title = "Splix.io";
-        }
-        //alert("Your rank was " + x);
+        setInterval(function() {
+            document.title = (document.title.startsWith("Kills: ")) ? ("Rank: " + curRank + " / " + totalRanks) : ("Kills: " + numKills);
+        }, 10000);
+        //on death notify ("Your rank was " + x);
     });
 })();
