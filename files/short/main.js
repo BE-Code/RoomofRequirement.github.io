17ac5bc61d0f634f1e8b9388f62559cf8be4d2a8
BE-Code <43590516+BE-Code@users.noreply.github.com>
2019-09-10 13:13:32 -0600

    Create main.js

---
+++ files/short/main.js
@@ -0,0 +1,6 @@
+var endpoint = "https://www.jsonstore.io/4854b44d8da280ff6d53fb4b66d7e536310bf40d793c9b8cee1675a34cba2336";
+
+function getrandom() {
+  var random_string = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
+  return random_string();
+}
