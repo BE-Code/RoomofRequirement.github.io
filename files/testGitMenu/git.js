3e7401791f5568579a9422b2b3670dae5141d500
BE-Code <43590516+BE-Code@users.noreply.github.com>
2019-09-18 11:23:43 -0600

    many minor changes
    ul instead of dl
    main function name change
    simplification and removal of redundancies

--- files/testGitMenu/git.js
+++ files/testGitMenu/git.js
@@ -1,27 +1,25 @@
-jQuery.githubUser = function(username, callback) {
-  jQuery.getJSON('https://api.github.com/repos/BE-Code/RoomofRequirement.github.io/contents',callback)
-  //jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
-  //jQuery.getJSON('https://api.github.com/repos/'+username+'/tempTest/contents)
+jQuery.githubUser = function(callback) {
+  jQuery.getJSON('https://api.github.com/repos/BE-Code/RoomofRequirement.github.io/contents',callback)  // For list of files in repository
 }
 
-jQuery.fn.loadRepositories = function() {
+jQuery.fn.loadDynamicMenu = function() {
   var username = "BE-Code";
-  this.html("<span>Querying GitHub for sites...</span>");
+  this.html("<span>Querying GitHub for pages...</span>");
   var target = this;
-  $.githubUser(username, function(data) {
-    //var fileList = data.data; // JSON Parsing
-    sortByName(data);    
-    
-    var list = $('<dl/>');
+  
+  $.githubUser(function(data) {
+    sortByName(data);
+    var list = $("<ul>");
     target.empty().append(list);
     $(data).each(function() {
       if (this.name.endsWith(".html")) {
-        list.append("<dt><a href=\"" + this.name.substring(0, this.name.length - 5) + "\">" + this.name.substring(0, this.name.length - 5) + "</a>");
-        list.append("<dd>Size: " + this.size + "</dd>");
+        var page = this.name.substring(0, this.name.length - 5);
+        list.append("<li><a href=\"" + page + "\">" + page + "</a></li>");
       }
-    });      
+    });
+    list.append("</ul>");
   });
-  
+
   function sortByName(repos) {
     repos.sort(function(a,b) {
       return a.name - b.name;
